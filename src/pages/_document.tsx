f1671c10f04676960b3ede6be5ac8e11f2ea66d9
Carlos Tonholi <carlos.tonholi@stoom.com.br>
2021-03-02 23:21:37 -0300

    feat(Countdown): Created

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-            rel="stylesheet`}
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
           />
         </Head>
-
         <body>
-          <Main />
-          <NextScript />
+          <Main></Main>
+          <NextScript></NextScript>
         </body>
       </Html>
     );
